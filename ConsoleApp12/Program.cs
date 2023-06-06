180004d0416e35b2b661f694b4094eb7cff33ffa
vitor <vitor.hag@outlook.com>
2023-06-06 19:29:23 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿double[] salarios = new double[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Digite o salário da {i + 1}° pessoa: ");
+    salarios[i] = double.Parse(Console.ReadLine());
+
+    if (salarios[i] < 1000)
+    {
+        salarios[i] *= 1.1;
+    }
+}
+
+Console.WriteLine("\nSalários atualizados:");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Pessoa {i + 1}: R${salarios[i]}");
+}
+
+Console.ReadKey();
+
